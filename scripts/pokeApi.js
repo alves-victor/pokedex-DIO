b13788dc38c5667ba8fd99ca59f9314a1257de4f
Victor Alves <victorhugo_sp4@hotmail.com>
2024-02-05 17:59:36 -0300

    add função para melhor manuseio dos dados dos pokemons por meio de uma classe

--- scripts/pokeApi.js
+++ scripts/pokeApi.js
@@ -1,11 +1,24 @@
 
 const pokeApi = {}
 
-pokeApi.getPokemonDetails = (pokemon) => {
-    return fetch(pokemon.url).then(response => response.json())
+function savePokemonDetail(pokemonDetail){
+    const pokemon = new Pokemon();
+    pokemon.number = pokemonDetail.order;
+    pokemon.name = pokemonDetail.name;
+    pokemon.types = pokemonDetail.types.map(types => types.type.name);
+    pokemon.mainType = pokemon.types[0];
+    pokemon.image = pokemonDetail.sprites.other.dream_world.front_default;
+    return pokemon;
 }
 
-pokeApi.getPokemons = function(offset = 0, limit = 10){
+pokeApi.getPokemonDetails = (pokemon) => {
+    return fetch(pokemon.url)
+    .then(response => response.json())
+    .then(savePokemonDetail)
+    .catch(error => console.error(error));
+}
+
+pokeApi.getPokemons = function(offset = 0, limit = 8){
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
     return fetch(url)
@@ -14,5 +27,5 @@ pokeApi.getPokemons = function(offset = 0, limit = 10){
     .then((pokemons) => pokemons.map(pokeApi.getPokemonDetails))
     .then((detailRequest) => Promise.all(detailRequest))
     .then((pokemonDetails) => pokemonDetails)
-    .catch((error) => console.error(error))
+    .catch((error) => console.error(error));
 }
